df610903d8bab41632f1b8f2d91f1546c6be089b
Chris Meehan <95490067+Chris7364@users.noreply.github.com>
2024-02-23 13:16:54 -0500

    added autoguess feature for search bar
    Updated home.HTML
    Updated pages\URLS.py
    Updated pages\views.py
    made these updates to implement the atoguess feature with the search bar

--- pages/views.py
+++ pages/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.http from django.http import JsonResponse
+from .models import CachedCharacters
 
 # Create your views here.
 
@@ -10,3 +12,13 @@ def forum(request):
 
 def loginpage(request):
     return render(request, "pages/Marvel_LoginPage.html", {})
+
+
+# Creates auto guess feature
+
+def character_name_suggestions(request):
+    query = request.GET.get('query', '')
+    suggestions = CachedCharacters.objects.fil;ter(name__icontains=query)[:3]  #Gets up to 3 suggestions
+    suggestion_list = [character.name for charachter in suggestions]
+    return JsonResponse({'suggestions': suggestion_list})
+    
